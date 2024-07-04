1746394dabb9876dfdd8c740a5cf6c1587316f73
unknown <hann.jang@labinfo.is.escuelaing.edu.co>
2024-07-04 12:32:20 -0500

    Terminado

---
+++ src/test/java/arsw/accessingdatamongodb/AccessingdatamongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package arsw.accessingdatamongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessingdatamongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
